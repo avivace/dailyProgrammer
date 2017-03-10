7b1855773aecd8d2e0304d47547a37009b4fcf38
Antonio Vivace <puma95.95@gmail.com>
2017-03-10 09:07:57 +0100

    Initial commit. Version 0.5

---
+++ js/chall.js
@@ -0,0 +1,93 @@
+/*
+      avivace/dailyprogrammer/challenge.js
+      twitter.com/avivace4 - www.avivace.ovh
+
+      Renders the challenge passed as query string i.
+
+      0.5
+*/
+
+// Pre-render escaped HTML
+function htmlDecode(input){
+   var e = document.createElement('div');
+   e.innerHTML = input;
+   return e.childNodes.lenght === 0 ? "" : e.childNodes[0].nodeValue;
+ }
+
+// Catch query strings
+function getParameterByName(name, url) {  
+     if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$.getJSON(
+  "https://www.reddit.com/r/dailyprogrammer/new.json",
+  function foo(data)
+  {
+    var ci = parseInt(getParameterByName('i'));
+    var cb = 0;
+    $.each (
+      data.data.children.slice(ci,ci+1),
+      function (i, post) {
+        $('#title2').append('Challenge #'+ post.data.title.substring(24,27));
+        $('#title').append('Challenge #'+ post.data.title.substring(24,27));
+
+        $('#problem').append(htmlDecode(post.data.selftext_html));
+        $('#problem').append('Challenge created by <b>' +post.data.author+'</b>');
+        // Solutions
+        var challengeJSON = "https://www.reddit.com/r/dailyprogrammer/comments/"+post.data.id+".json?sort=top";
+        $.getJSON(
+          challengeJSON,
+          function appendComments(comments)
+          {
+            var sol_i = 3;    // Numbers of solution to show
+            //Slice & Print
+            for (var i = 0; i <sol_i; i++) {
+
+              $('#solution').append("<a style='color: black; font-size: 24px'>Solution "+(i+1)+"</a> by ");
+              $('#solution').append(htmlDecode(comments[1].data.children[i].data.author));
+              $('#solution').append(htmlDecode(comments[1].data.children[i].data.body_html));
+              if (i < sol_i-1)
+                $('#solution').append("<br> <hr> <br>");
+              
+            }
+          })
+        
+      }
+    )
+
+    // Rewrite relative reddit links as absolutes (and open it in a new activity)
+    var a_elements = document.getElementsByTagName("a");
+    for (var i = 0; i < a_elements.length; i++) {
+    //a_elements.forEach(){
+       // console.log(a_elements[i].innerHTML);
+       if (/\/[\w]\/[\w]*/.test(a_elements[i].innerHTML)){
+         console.log("relative link trigger");
+         a_elements[i].href='https://reddit.com'+a_elements[i].innerHTML;
+         a_elements[i].target='_blank';
+         console.log(a_elements[i]);
+       }
+    }
+
+    // TODO: really?
+    setTimeout( function(){ 
+          $('pre code').each(function(i, block) {
+             hljs.highlightBlock(block);
+            cb ++;
+          });
+          if (cb < 5){
+            console.log("Retrying code highlight in 2 seconds.");
+            setTimeout( function(){ 
+              $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+                });
+            }, 2000 );
+          }
+    }  , 1000 );
+
+})
---
+++ js/front.js
@@ -0,0 +1,168 @@
+/*
+      avivace/dailyprogrammer/front.js
+      twitter.com/avivace4 - www.avivace.ovh
+
+      Renders the challenge list, with title and a description peek for every
+      challenge.
+
+      0.4
+*/
+
+// Markdown to plain text (.selftext)
+function removeMd(md, options) {
+  options = options || {};
+  options.stripListLeaders = options.hasOwnProperty('stripListLeaders') ? options.stripListLeaders : true;
+  options.gfm = options.hasOwnProperty('gfm') ? options.gfm : true;
+
+  var output = md;
+  try {
+    if (options.stripListLeaders) {
+      output = output.replace(/^([\s\t]*)([\*\-\+]|\d\.)\s+/gm, '$1');
+    }
+    if (options.gfm){
+      output = output
+        // Header
+        .replace(/\n={2,}/g, '\n')
+        // Strikethrough
+        .replace(/~~/g, '')
+        // Fenced codeblocks
+        .replace(/`{3}.*\n/g, '');
+    }
+    output = output
+      // Remove HTML tags
+      .replace(/<(.*?)>/g, '$1')
+      // Remove setext-style headers
+      .replace(/^[=\-]{2,}\s*$/g, '')
+      // Remove footnotes?
+      .replace(/\[\^.+?\](\: .*?$)?/g, '')
+      .replace(/\s{0,2}\[.*?\]: .*?$/g, '')
+      // Remove images
+      .replace(/\!\[.*?\][\[\(].*?[\]\)]/g, '')
+      // Remove inline links
+      .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+      // Remove Blockquotes
+      .replace(/>/g, '')
+      // Remove reference-style links?
+      .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, '')
+      // Remove atx-style headers
+      .replace(/^\#{1,6}\s*([^#]*)\s*(\#{1,6})?/gm, '$1')
+      .replace(/([\*_]{1,3})(\S.*?\S)\1/g, '$2')
+      .replace(/(`{3,})(.*?)\1/gm, '$2')
+      .replace(/^-{3,}\s*$/g, '')
+      .replace(/`(.+?)`/g, '$1')
+      .replace(/\n{2,}/g, '\n\n');
+  } catch(e) {
+    console.error(e);
+    return md;
+  }
+  return output;
+};
+
+// Pre render HTML (.selftext_html)
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+    return e.childNodes.lenght === 0 ? "" : e.childNodes[0].nodeValue;
+  }
+
+$.getJSON(
+  "https://www.reddit.com/r/dailyprogrammer/new.json",
+  function foo(data)
+  {
+    $.each(
+      data.data.children.slice(0, 100),
+      function (i, post) {
+
+        if (post.data.title.indexOf('Challenge') !== -1 || post.data.title.indexOf('Weekly') !== -1){
+          var card_style = 'red';
+          var diff = 'HARD';
+          if (post.data.title.indexOf('Easy') !== -1) {
+            card_style='green';
+            diff='EASY';
+          } else if (post.data.title.indexOf('Intermediate') !== -1) {
+            card_style='orange';
+            diff='MEDIUM';
+          } else if (post.data.title.indexOf('Weekly') !== -1) {
+            card_style='blue';
+            diff='WEEKLY';
+          }
+          
+          var link = 'chall.html?i='+i;
+          var header = post.data.title.substring(27, post.data.title.lenght).replace("[Easy]", '').replace("[Easy/Intermediate]", '').replace("[Hard]", '').replace("[Intermediate]", '');
+          var body = removeMd(post.data.selftext).replace('Description','').substring(0,300)+ ' ...';
+
+          // Weekly
+          if (card_style == 'blue') header = post.data.title.substring(13, post.data.title.lenght);
+
+          // border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+
+          // Let's build the card
+
+
+          if (card_style == 'white'){
+            var card_color = 'white';
+            var text_color = 'black-text';
+          }
+          else if (card_style == 'red') {
+            var card_color = 'white';
+            var text_color = 'black-text';
+            var diff_col = '#F22613'; 
+          }
+          else if (card_style == 'green') {
+            var diff_col = '#26C281'; 
+            var card_color = 'white';
+            var text_color = 'black-text';
+
+          }
+          else if (card_style == 'orange') {
+            var diff_col = '#D35400'; 
+            var card_color = 'white';
+            var text_color = 'black-text';
+          }
+          else if (card_style == 'blue') {
+            var diff_col = 'blue'; 
+            var card_color = 'white';
+            var text_color = 'black-text';
+          }
+
+          var p1 = '<div class="card ';
+
+          var card_o = p1+card_color+'">  <div class="card-content" style="border-bottom: 1px solid rgba(0, 0, 0, 0.12)"><span class="card-title '+text_color+'">'+header+'</span><p class="'+text_color+'">'+body+'</p></div><div class="card-action"><a href="'+link+'" style="font-weight: 500;" class="'+text_color+'">GO TO CHALLENGE &nbsp;<i style="vertical-align: -15%; font-size: 16px;" class="material-icons">launch</i></a>  <a style="font-size: 14px ;color:'+ diff_col +'; position: absolute; right: 0px"><b> '+diff+'</b> </a></div></div>';
+
+          var card = p1+card_color+'">  <div class="card-content" style="border-bottom: 1px solid rgba(0, 0, 0, 0.12)"><span class="card-title '+text_color+'"><a  style="color: black; text-shadow: 1px 1px 2px #BBB;">'+ header+'</a><a style="font-size: 14px ;color:'+ diff_col +'; position: absolute; right: 3%"><b> '+diff+'</b> </a></span><p class="'+text_color+'">'+body+'</p></div></div></div>';
+
+          $("#all").append('<div id="'+i+'">'+card+'</div>');
+          $("#"+i).click(function(e){
+            //e.preventDefault();
+            window.location = link;    
+          });
+          
+
+          var card_nodiff = p1+card_color+'">  <div class="card-content" style="border-bottom: 1px solid rgba(0, 0, 0, 0.12)"><span class="card-title '+text_color+'">'+header+'<a style="font-size: 14px ;color:'+ diff_col +'; position: absolute; right: 3%"><b> </b> </a></span><p class="'+text_color+'">'+body+'</p></div></div></div>';
+
+
+          if (post.data.title.indexOf('Easy') !== -1) {
+            $("#easy").append('<div id="a'+i+'">'+card_nodiff+'</div>');
+          } else if (post.data.title.indexOf('Intermediate') !== -1) {
+            $("#med").append('<div id="a'+i+'">'+card_nodiff+'</div>');
+          } else if (post.data.title.indexOf('Hard') !== -1) {
+            $("#hard").append('<div id="a'+i+'">'+card_nodiff+'</div>');
+          }
+
+          $("#a"+i).click(function(e){
+            //e.preventDefault();
+            window.location = link;    
+          });
+
+
+          /*$("#cc").append('<div class="row">  <div class="col s12 m6"><div class="card blue-grey darken-1">  <div class="card-content white-text">');
+          $("#cc").append('<span class="card-title">'+ header + '</span>');
+          $("#cc").append('<p>Description PLACEHOLDER</p> </div> <div class="card-action"> <a href="#">GO TO CHALLENGE</a> </div> </div>  </div> </div>');
+
+          $("#cards").append('	<div class="card card-'+diff_col+'"> <div class="card-main"> <div class="card-inner"> <p class="card-heading" id="heading2">'+ header +'</p> <p>'+ body + '</p> </div> <div class="card-action"> <div class="card-action-btn pull-left"> <a class="btn btn-flat waves-attach" href="'+ link +'">&nbsp;GO TO CHALLENGE<span class="icon margin-left-sm">open_in_new</span></a></div></div></div> </div>');*/
+        }
+
+      }
+    )
+  }
+)
